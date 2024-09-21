37a846538452dc88de104f6a3974109bedacfb7b
Stefan <turner.stefan281@gmail.com>
2024-09-21 13:23:46 +0200

    update style

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,9 @@
+//Hamburger event listener
 document.querySelector('.hamburger').addEventListener('click', (e) => {
 	e.target.classList.toggle('hamburger--open');
 	e.target.previousElementSibling.classList.toggle('navigation--open');
 });
+
+//Footer Year
+document.querySelector('.footer__copy--year').innerText =
+	new Date().getFullYear();
