e655282058ca6e14f0f24458b5828b47cfdfc0d4
Stefan <turner.stefan281@gmail.com>
2024-09-22 20:07:42 +0200

    add intersection observer for title

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,3 +7,23 @@ document.querySelector('.hamburger').addEventListener('click', (e) => {
 //Footer Year
 document.querySelector('.footer__copy--year').innerText =
 	new Date().getFullYear();
+
+//Frontpage Works Title Animation Observer
+const titles = document.querySelectorAll('.fp-works__work--title');
+const titleOptions = { threshold: 0, rootMargin: '0% 0% -50% 0%' };
+
+const titleObserver = new IntersectionObserver((entries, observer) => {
+	entries.forEach((title) => {
+		if (title.isIntersecting) {
+			console.log('in');
+			title.target.classList.add('fp-works__work--title--in');
+		} else {
+			console.log('out');
+			title.target.classList.remove('fp-works__work--title--in');
+		}
+	});
+}, titleOptions);
+
+titles.forEach((title) => {
+	titleObserver.observe(title);
+});
