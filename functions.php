fb11646f6c7a76baa8c578706ac80ea7ad55f345
Stefan <turner.stefan281@gmail.com>
2024-10-11 16:38:52 +0200

    add function only load contact form scripts when needed

--- functions.php
+++ functions.php
@@ -103,3 +103,23 @@ function my_init() {
   }
 }
 add_action('init', 'my_init');
+
+/**
+ * Only load wpcf7 scripts and style on contact page
+ */
+
+ add_filter( 'wpcf7_load_js', '__return_false' );
+ add_filter( 'wpcf7_load_css', '__return_false' );
+
+add_action('wp_enqueue_scripts', 'load_wpcf7_scripts');
+
+function load_wpcf7_scripts() {
+  if ( is_page('20') ) {
+    if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+      wpcf7_enqueue_scripts();
+    }
+    if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
+      wpcf7_enqueue_styles();
+    }
+  }
+}
